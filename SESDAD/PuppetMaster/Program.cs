30cde4f4d74f5cfe3614aa11ca3de430e78c01be
rnjesuz <rn_jesuz@hotmail.com>
2015-10-19 16:27:53 +0100

    parser

--- SESDAD/PuppetMaster/Program.cs
+++ SESDAD/PuppetMaster/Program.cs
@@ -10,6 +10,38 @@ namespace PuppetMaster
     {
         static void Main(string[] args)
         {
+            string input;
+            List<string> inputParsed;
+            input = Console.ReadLine();
+            inputParsed = ParseInput(input);
+            string first = inputParsed.First();
+            switch (first)
+            {
+                case "Subscriber":
+                    string processname = inputParsed.ElementAt(1);
+                    string topicname = inputParsed.ElementAt(3);
+
+                    break;
+                case "Publisher":
+                    break;
+                case "Status":
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
         }
+
+        private static List<string> ParseInput(string input)
+        {
+            List<string> parse = new List<string>();
+            string[] parsedBySpace = input.Split(null);
+            parse = parsedBySpace.OfType<string>().ToList();
+
+            return parse;
+
+        }
+
+
     }
 }
