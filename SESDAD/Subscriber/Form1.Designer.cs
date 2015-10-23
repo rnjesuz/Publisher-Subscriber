8e820ad3f68041a46f4f211a3efada98933ad859
rnjesuz <rn_jesus@hotmail.com>
2015-10-23 18:25:44 +0100

    creted subscriber remote object and form methods

--- SESDAD/Subscriber/Form1.Designer.cs
+++ SESDAD/Subscriber/Form1.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.SubscribeButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TopicBox = new System.Windows.Forms.TextBox();
+            this.PublicationBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // SubscribeButton
@@ -38,25 +39,34 @@
             this.SubscribeButton.Name = "SubscribeButton";
             this.SubscribeButton.Size = new System.Drawing.Size(75, 23);
             this.SubscribeButton.TabIndex = 0;
-            this.SubscribeButton.Text = "Enter Topic to Subscribe";
+            this.SubscribeButton.Text = "Subscribe";
             this.SubscribeButton.UseVisualStyleBackColor = true;
-            this.SubscribeButton.Click += new System.EventHandler(this.button1_Click);
+            this.SubscribeButton.Click += new System.EventHandler(this.SubscribeButton_Click);
             // 
-            // textBox1
+            // TopicBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(179, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Enter Topic to Subscribe";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.TopicBox.Location = new System.Drawing.Point(12, 12);
+            this.TopicBox.Name = "TopicBox";
+            this.TopicBox.Size = new System.Drawing.Size(179, 20);
+            this.TopicBox.TabIndex = 1;
+            this.TopicBox.TextChanged += new System.EventHandler(this.TopicBox_TextChanged);
+            // 
+            // PublicationBox
+            // 
+            this.PublicationBox.Location = new System.Drawing.Point(13, 39);
+            this.PublicationBox.Multiline = true;
+            this.PublicationBox.Name = "PublicationBox";
+            this.PublicationBox.Size = new System.Drawing.Size(259, 210);
+            this.PublicationBox.TabIndex = 2;
+            this.PublicationBox.TextChanged += new System.EventHandler(this.PublicationBox_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PublicationBox);
+            this.Controls.Add(this.TopicBox);
             this.Controls.Add(this.SubscribeButton);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -69,7 +79,8 @@
         #endregion
 
         private System.Windows.Forms.Button SubscribeButton;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TopicBox;
+        private System.Windows.Forms.TextBox PublicationBox;
     }
 }
 
--- SESDAD/Subscriber/Program.cs
+++ SESDAD/Subscriber/Program.cs
@@ -1,22 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SESDAD
 {
-    static class Program
+    static class Subscriber
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+
+        internal static BrokerInterface broker;
+        internal static Form1 form;
         [STAThread]
         static void Main()
         {
+            //hardcoded port to 8090
+            //TODO change port to be dynamic
+            TcpChannel channel = new TcpChannel(8090);
+            ChannelServices.RegisterChannel(channel, false);
+
+            RemoteSubscriber rs = new RemoteSubscriber();
+            RemotingServices.Marshal(rs, "SubscriberServer", typeof(RemoteSubscriber));
+
+            broker = (BrokerInterface)Activator.GetObject(
+                typeof(BrokerInterface),
+                "tcp://localhost:8086/BrokerServer");
+
+            try
+            {
+                broker.ConnectSubscriber();
+            }
+            catch (SocketException)
+            {
+                System.Console.WriteLine("Could not locate server");
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            form = new Form1();
+            Application.Run(form);
         }
     }
+
+
+    delegate void DelegateReceivePublication(string message);
+
+    public class RemoteSubscriber : MarshalByRefObject, SubscriberInterface
+    {
+        public static Form1 form = Subscriber.form;
+        private BrokerInterface broker = Subscriber.broker;
+
+        public void ReceivePublication(string publication)
+        {
+            form.Invoke(new DelegateReceivePublication(form.UpdatePublication), publication);
+        }
+
+        internal void AddSubscription(string topic)
+        {
+            try
+            {
+                broker.AddSubscription(this, topic);
+            }
+            catch (SocketException)
+            {
+                System.Console.WriteLine("Could not locate server");
+            }
+        }
+    }
+
+
 }
