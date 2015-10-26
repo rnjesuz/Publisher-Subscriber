a8fd061b5fd6faecdb01d0b36a4f11f9ad02c13a
rnjesuz <rn_jesuz@hotmail.com>
2015-10-26 10:56:15 +0000

    same as previous commit

--- SESDAD/Broker/Program.cs
+++ SESDAD/Broker/Program.cs
@@ -17,14 +17,7 @@ namespace SESDAD
         [STAThread]
         static void Main()
         {
-
-            BinaryServerFormatterSinkProvider provider = new BinaryServerFormatterSinkProvider();
-            provider.TypeFilterLevel = TypeFilterLevel.Full;
-            IDictionary props = new Hashtable();
-            props["port"] = 8086;
-            TcpChannel channel = new TcpChannel(props, null, provider);
-
-            //TcpChannel channel = new TcpChannel(8086);
+            TcpChannel channel = new TcpChannel(8086);
             ChannelServices.RegisterChannel(channel, false);
 
             RemotingConfiguration.RegisterWellKnownServiceType(
