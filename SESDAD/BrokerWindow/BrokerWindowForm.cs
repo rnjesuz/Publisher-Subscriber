a5bca0b8dedae7742f860b53bc613b99f4c2fcdb
rnjesuz <rn_jesuz@hotmail.com>
2015-11-05 16:29:14 +0000

    added old broker in windowsform

---
+++ SESDAD/BrokerWindow/BrokerWindowForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrokerWindow
+{
+    public partial class BrokerWindowForm : Form
+    {
+        public BrokerWindowForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
